1b4cba9c4bb13cd3a9d251789522894ff3a064b9
Mushfiqur Niloy <mushfiqur06@gmail.com>
2022-06-17 21:38:16 +0600

    Show Categories

--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,58 +1,22 @@
-import React from "react";
-import { PRODUCTS, SET_API, SET_LOADING } from "../constants/products";
-import { getAllProductsAction } from "../request/product";
-
-function productReducer(state: any, action: any) {
-  switch (action.type) {
-    case SET_API:
-      return { ...state, url: action.payload };
-    case PRODUCTS:
-      return { ...state, products: action.payload };
-    case SET_LOADING:
-      return { ...state, loading: action.payload };
-    default:
-      return state;
-  }
-}
+import { useCategories } from "../hooks/useCategories";
+import Category from "../module/categories/categoy";
 
 export default function Home() {
-  const [{ url, products, loading }, dispatch] = React.useReducer(
-    productReducer,
-    {
-      url: "",
-      products: [],
-      loading: true,
-    }
-  );
-  const dispatchSetUrl = (value: string) => {
-    dispatch({ type: SET_API, payload: value });
-  };
-
-  React.useEffect(() => {
-    dispatchSetUrl("https://fakestoreapi.com/products");
-    const fetchData = async () => {
-      dispatch({ type: SET_LOADING, payload: true });
-      const res = await getAllProductsAction(url);
-      dispatch({ type: PRODUCTS, payload: res.data });
-      dispatch({ type: SET_LOADING, payload: false });
-    };
-    fetchData();
-  }, []);
-  console.log(products);
+  const { categories, loading } = useCategories();
 
   return (
     <div>
-      {loading ? (
-        <div>Loading</div>
-      ) : products.length === 0 ? (
-        <div>Products Not Found</div>
-      ) : (
-        products.map((value: any, index: number) => (
-          <div key={index}>
-            <h2>{value.title}</h2>
+      <div className="container mx-auto p-4">
+        {loading ? (
+          <div>Loading</div>
+        ) : (
+          <div className="grid grid-cols-4 gap-4">
+            {categories?.map((category: string, index: number) => (
+              <Category key={index} category={category} />
+            ))}
           </div>
-        ))
-      )}
+        )}
+      </div>
     </div>
   );
 }
